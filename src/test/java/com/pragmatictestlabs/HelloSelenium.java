f25490c40726cfe9f4f9cc08bc6bc20f840a8e2a
janesh.kodikara <janesh.kodikara@cammsgroup.com>
2024-03-29 05:27:06 +0530

    Initial commit

---
+++ src/test/java/com/pragmatictestlabs/HelloSelenium.java
@@ -0,0 +1,45 @@
+package com.pragmatictestlabs;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HelloSelenium {
+
+
+    @Test
+    public void helloSelenium(){
+
+        //Setup web browser driver (chrome driver)
+        WebDriverManager.chromedriver().setup();
+
+        //Open a web browser (Chrome browser)
+        WebDriver driver = new ChromeDriver();
+
+
+        //Navigate to the login screen (Type URL https://www.saucedemo.com/)
+        driver.get("https://www.saucedemo.com/");
+
+        //Type username standard_user
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+
+        //Type password secret_sauce
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        //Click the login
+        driver.findElement(By.id("login-button")).click();
+
+
+        //Verify/check the Product label is available in the landing page after login
+        Assert.assertEquals( driver.findElement(By.cssSelector("span.title")).getText() ,"Products");
+
+
+        //Close the browser
+        driver.quit();
+
+    }
+
+}
