b93de66ecec83c68d5cb1f9758c67a93b2f8e460
janesh.kodikara@cammsgroup.com <janesh.kodikara@cammsgroup.com>
2024-04-20 20:53:00 +0530

    Add synchronisation examples

--- src/test/java/com/pragmatictestlabs/day4/IntroductionToSyncTest.java
+++ src/test/java/com/pragmatictestlabs/day4/IntroductionToSyncTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,12 +15,13 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class IntroductionToSyncTest {
-
     private WebDriver driver;
-
     @BeforeMethod
     public void beforeMethod(){
         WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.navigate().to("https://eviltester.github.io/synchole/collapseable.html");
+
     }
 
     @AfterMethod
@@ -28,10 +31,6 @@ public class IntroductionToSyncTest {
 
     @Test
     public void testCollapseDiv() throws InterruptedException {
-
-        driver = new ChromeDriver();
-        driver.navigate().to("https://eviltester.github.io/synchole/collapseable.html");
-
         //Click the section
         driver.findElement(By.cssSelector("section.condense")).click();
 
@@ -39,14 +38,42 @@ public class IntroductionToSyncTest {
         //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); Deprecated  and avoid using
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+        //Click the About link
+        driver.findElement(By.cssSelector("a#aboutlink")).click();
+
+        //Verify that the URL contains about.html
+        Assert.assertTrue(driver.getCurrentUrl().contains("about.html"));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+
+    }
+
+    @Test
+    public void testWithExplicitWait() throws InterruptedException {
+        //Click the section
+        driver.findElement(By.cssSelector("section.condense")).click();
+
+
+        //Explicit Wait
+        //Wait till the link is visible
+        new WebDriverWait(driver, Duration.ofSeconds(5), Duration.ofMillis(50))
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("a#aboutlink")));
+
 
         //Click the About link
         driver.findElement(By.cssSelector("a#aboutlink")).click();
 
-
         //Verify that the URL contains about.html
         Assert.assertTrue(driver.getCurrentUrl().contains("about.html"));
+
+
     }
+
+
+
+
+
+
 
 
 }
